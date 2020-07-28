03d6539fbe276a1de4fc67ad85123beb2e643100
github username <github email address>
2020-07-28 12:58:07 -0700

    Done.

--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,17 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+    attr_reader :number
+
+    def initialize(limit)
+        @number = findprime(limit)
+    end
+
+    def findprime(limit)
+        prime = limit
+        (2...Math.sqrt(limit).to_i).each do |i|
+            break if prime <= 1
+            prime /= i while(prime > i && prime%i == 0)
+        end
+        prime
+    end
+end
